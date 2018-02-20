76f00609f3d5baa616ab91408a5f476b7d167c02
N Venkateshan <venkycse93@gmail.com>
2018-02-21 00:00:11 +0530

    removed the keys

--- app/config.php
+++ app/config.php
@@ -3,9 +3,9 @@
 return [
 
 	"s3" => [
-		"key" => "AKIAI66SOVPGYD5ROJKQ",
-		"secret" => "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",
-		"bucket" => "sciencemarket.org"
+		"key" => "",
+		"secret" => "",
+		"bucket" => ""
 	]
 	
 ];
