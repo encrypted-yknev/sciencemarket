06cad431ba4be655c159021ddf2f9d41e652a33a
venky <venkycse93@gmail.com>
2018-05-18 03:01:25 +0530

    used group by in the query to avoid duplicate posts

--- forum/myposts/index.php
+++ forum/myposts/index.php
@@ -55,6 +55,7 @@ $qstn_array=$qstn_arr_str="";
                     left outer join groups g
                     on g.group_id = a1.parent_group_id
 					where a.posted_by='".$_SESSION['user']."' 
+                    group by a.qstn_id,a.qstn_titl,a.qstn_desc,a.posted_by,a.topic_id,a.up_votes,a.down_votes,a.created_ts,a1.parent_group_id,g.group_nm
                     order by a.created_ts desc limit 10";
 					
 					include "../fetch_answers1.php";
@@ -67,6 +68,7 @@ $qstn_array=$qstn_arr_str="";
 					$sql_fetch_all_qstn = "select qstn_id
 										from questions 
 										where posted_by='".$_SESSION['user']."' 
+                                        group by a.qstn_id,a.qstn_titl,a.qstn_desc,a.posted_by,a.topic_id,a.up_votes,a.down_votes,a.created_ts,a1.parent_group_id,g.group_nm
 										order by created_ts desc
 										";
 					foreach($conn->query($sql_fetch_all_qstn) as $row_qid)	{
