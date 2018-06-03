6e387ffdf054196462b6c6d7ad20bc1be3a37bab
venky <venkycse93@gmail.com>
2018-06-03 11:30:28 +0530

    remove merge conflict

---
+++ connectDb.php
@@ -0,0 +1,18 @@
+<?php
+
+/*	Connection parameters to connect to mysqlDb */
+$username="root";
+$password="mysqlroot";
+$servername="localhost";
+$dbname = "smdev_test"; //Set your database name here. No hardcoding.
+
+try	{
+	$conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e)	{
+	echo "Error in connection. Please try after some time.";
+}
+
+?>
